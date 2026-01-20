49a99f3bd1b47950524eaf4b2641f254ac324fad
kyohashi <koya.ohashi@gmail.com>
2026-01-21 00:02:43 +0900

    update dependencies

--- R/MRDLTM-package.R
+++ R/MRDLTM-package.R
@@ -1,9 +1,5 @@
 ## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
 #' @useDynLib MRDLTM, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
