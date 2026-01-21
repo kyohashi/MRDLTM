d7b599d4db8ad59350528708ba10db0875c271fa
kyohashi <koya.ohashi@gmail.com>
2026-01-22 01:51:46 +0900

    add beta sampler

---
+++ R/sampler_beta.R
@@ -0,0 +1,57 @@
+#' Sample Response Coefficients beta_zi
+#'
+#' @description
+#' Sample the topic-item specific response coefficients beta_zi
+#'
+#' @param active_data A data frame of active observations (c, i, t)
+#' @param state An environment containing the current MCMC state
+#' @param x_it An array of marketing covariates
+#'
+#' @return NULL
+#' @importFrom MASS mvrnorm
+#' @noRd
+sample_beta = function(active_data, state, x_it){
+  n_topic = dim(state$beta_zi)[1]
+  n_item = dim(state$beta_zi)[2]
+  n_var = dim(state$beta_zi)[3]
+
+  # Loop through (z, i) combination
+  for (z in 1:n_topic){
+    for (i in 1:n_item){
+      # identify corresponding indices for (z, i)
+      idx = which(active_data$item == i & state$z_cit == z)
+
+      # --- get required statistics ---
+      ## priors
+      mu_i = state$mu_i[i, ]
+      V_i_inv = solve(state$V_i[i,,])
+
+      if (length(idx) == 0) {
+        # If no observations, sample from the hierarchical prior
+        state$beta_zi[z, i, ] = as.vector(
+          mvrnorm(1, mu = mu_i, Sigma = state$V_i[i, , ])
+        )
+        next
+      }
+
+      ## utility
+      u_zi = state$u_cit[idx]
+
+      ## X_zi
+      times = active_data$time[idx]
+      X_zi = matrix(0, nrow = length(idx), ncol = n_var)
+      for (k in 1:length(idx))
+        X_zi[k,] = x_it[i, times[k], ]
+
+      # --- posterior parameters ---
+      R = t(X_zi) %*% X_zi + V_i_inv
+      R_inv = solve(R)
+      post_mean = R_inv %*% (t(X_zi) %*% u_zi + V_i_inv %*% mu_i)
+
+      # --- sample from poserior ---
+      state$beta_zi[z, i, ] = as.vector(
+        mvrnorm(1, mu = post_mean, Sigma = R_inv)
+      )
+    }
+  }
+}
---
+++ tests/testthat/test-sampler_beta.R
@@ -0,0 +1,50 @@
+test_that("sample_beta updates coefficients based on latent utility", {
+  # --- Setup ---
+  # Simple case: 1 customer, 1 item, 2 time points, 1 marketing var
+  active_data = data.frame(
+    cust = c(1, 1),
+    item = c(1, 1),
+    time = c(1, 2),
+    y_cit = c(1, 1)
+  )
+
+  n_topic = 2
+  n_item = 1
+  n_var = 1
+
+  state = init_state(
+    active_data = active_data,
+    n_item = n_item,
+    n_cust = 1,
+    n_topic = n_topic,
+    length_time = 2,
+    n_var = n_var,
+    p_dim = 1
+  )
+
+  # Assign all observations to Topic 1
+  state$z_cit = c(1, 1)
+
+  # Set positive latent utilities to pull beta upwards
+  state$u_cit = c(5.0, 5.0)
+
+  # Constant covariate (e.g., intercept only)
+  x_it = array(1, dim = c(1, 2, 1))
+
+  # Store initial values
+  initial_beta = state$beta_zi + 0
+
+  # --- Execution ---
+  sample_beta(active_data, state, x_it)
+
+  # --- Verification ---
+  # beta_zi should be updated
+  expect_false(identical(state$beta_zi, initial_beta))
+
+  # Topic 1's beta should be positive due to high utility
+  expect_gt(state$beta_zi[1, 1, 1], 0)
+
+  # Topic 2 has no data, so it should be sampled from prior N(0, 1)
+  # Just checking it exists and is numeric
+  expect_true(is.numeric(state$beta_zi[2, 1, 1]))
+})
